d07f03c46d56f24802719266c5bb954f05d7975b
Onyedikareal <frankonyeka25@gmail.com>
2023-01-31 02:37:02 +0100

    update: added pin verification buttomsheet
    For verifying user email when requesting for new password

---
+++ lib/app/features/signin/presentation/widgets/pin_bottomsheet.dart
@@ -0,0 +1,93 @@
+import 'dart:ui';
+
+import 'package:clinaj/app/features/signin/presentation/controllers/signin_controller.dart';
+import 'package:clinaj/app/features/signin/presentation/widgets/pin_themes.dart';
+import 'package:clinaj/app/features/signin/presentation/widgets/reset_password_bottomsheet.dart';
+import 'package:clinaj/core/general_widgets/buttom_sheet_borderline.dart';
+import 'package:clinaj/core/general_widgets/button_widget.dart';
+import 'package:clinaj/core/general_widgets/custom_list_space.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:pinput/pinput.dart';
+
+class VerifyPinBottomSheet extends StatelessWidget {
+  const VerifyPinBottomSheet({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    TextTheme primaryTextTheme = Theme.of(context).primaryTextTheme;
+    return BackdropFilter(
+      filter: ImageFilter.blur(sigmaX: 5.0, sigmaY: 5.0),
+      child: Container(
+        padding: const EdgeInsets.all(12),
+        height: 350,
+        decoration: const BoxDecoration(
+          borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(16), topRight: Radius.circular(16)),
+          color: Colors.white,
+        ),
+        child: ListView(
+          children: [
+            const SizedBox(
+              height: 4,
+            ),
+            const Align(
+                alignment: Alignment.topCenter, child: BottomSheetBorderLine()),
+            const CustomListSpacing(isMajorSpacing: true),
+            Text(
+              'Enter 4 Digits Code',
+              style: primaryTextTheme.headline2,
+            ),
+            const CustomListSpacing(isMajorSpacing: false),
+            Text(
+              """Enter the 4 digits code that you received on
+your email.""",
+              style: primaryTextTheme.bodyText1,
+            ),
+            const CustomListSpacing(isMajorSpacing: true),
+            GetBuilder<SigninController>(
+              builder: (_) {
+                return Pinput(
+                  length: 4,
+                  defaultPinTheme: defaultPinTheme,
+                  focusedPinTheme: focusedPinTheme,
+                  submittedPinTheme: submittedPinTheme,
+                  errorPinTheme: errorPinTheme,
+                  senderPhoneNumber: '070390938398',
+                  validator: (s) {
+                    return s == '2343' ? null : 'Incorrect pin';
+                  },
+                  pinputAutovalidateMode: PinputAutovalidateMode.onSubmit,
+                  showCursor: true,
+                  onCompleted: (pin) {
+                    _.verifyPin(pin: pin);
+                  },
+                );
+              },
+            ),
+            const CustomListSpacing(isMajorSpacing: true),
+            Center(
+              child: GetBuilder<SigninController>(
+                builder: (_) {
+                  return CustomButton(
+                      label: 'Continue',
+                      onPressed: () {
+                        _.goToPResetPassword(
+                            resetPwBottomsheet:const ResetPassword());
+                      },
+                      radius: 12,
+                      width: 345,
+                      borderColor: Theme.of(context).primaryColor,
+                      backgroundColor: Theme.of(context).primaryColor,
+                      textColor: const Color(0xffffffff),
+                      primaryTextTheme: primaryTextTheme);
+                },
+              ),
+            ),
+            const CustomListSpacing(isMajorSpacing: true),
+          ],
+        ),
+      ),
+    );
+  }
+}
